dc3c6b01f1489ccb5c65082bd3a316e1b4560f2b
Sivanishanthar Ram <r.nishanthar@gmail.com>
2024-02-26 20:56:32 -0500

    seed initial

--- seeds/movieData.js
+++ seeds/movieData.js
@@ -1,10 +1,47 @@
-const { Movie } = require('../models');
+const { Cast } = require('../models');
 
 const movieData = [
     {
-        title: '',
-        year: '',
-        genre_id: '',
+        title: 'Star Wars',
+        year: '1977-05-25',
+        genre_id: 1,
         cast_members: [],
-    }
+    },
+    {
+        title: 'The shawshank Redemption',
+        year: '1994-05-20',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'The Godfather',
+        year: '1972-06-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'The Dark Knight',
+        year: '2008-06-15',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: '12 Angry Men',
+        year: '1957-02-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'Pulp Fiction',
+        year: '1994-05-18',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'Fight Club',
+        year: '1999-06-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+
 ]
