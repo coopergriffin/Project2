b02fbf35d355837dee730c54a6489a437ee6ab9a
Sivanishanthar Ram <r.nishanthar@gmail.com>
2024-02-29 18:48:40 -0500

    seed initial bulk data create

---
+++ seeds/questionData.js
@@ -0,0 +1,47 @@
+const { Question } = require('../models');
+
+const questionData = [
+    {
+        title: 'Star Wars',
+        year: '1977-05-25',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'The shawshank Redemption',
+        year: '1994-05-20',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'The Godfather',
+        year: '1972-06-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'The Dark Knight',
+        year: '2008-06-15',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: '12 Angry Men',
+        year: '1957-02-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'Pulp Fiction',
+        year: '1994-05-18',
+        genre_id: 1,
+        cast_members: [],
+    },
+    {
+        title: 'Fight Club',
+        year: '1999-06-01',
+        genre_id: 1,
+        cast_members: [],
+    },
+
+]
