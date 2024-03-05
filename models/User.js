a539a76c18b9d0fdcdd57190aa6d15ce6dad37d9
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-03-05 10:22:45 -0500

    breakpoint

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {};
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -39,7 +44,7 @@ User.init(
         },
         sequelize,
         freezeTableName: true,
-        underscore: true,
+        underscored: true,
         modelName: 'user',
     }
 );
